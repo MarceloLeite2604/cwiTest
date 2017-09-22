886500581367c264628e13fbfe10cd0dc3245ea7
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-09-22 01:14:47 -0300

    Concluded an initial version of the project.

--- cwiTest/src/org/marceloleite/cwitest/CWITest.java
+++ cwiTest/src/org/marceloleite/cwitest/CWITest.java
@@ -10,11 +10,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.InvalidParameterException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+/**
+ * This is the class I created to answer the CWI test.
+ * 
+ * @author Marcelo Leite
+ *
+ */
 public class CWITest {
 
 	/**
@@ -33,20 +44,27 @@ public class CWITest {
 	private static final int FILE_READING_BUFFER_SIZE = 1024;
 
 	/**
+	 * The character used to separate values on the CSV file.
+	 */
+	private static final String CSV_SEPARATION_CHARACTER = ";";
+
+	/**
 	 * Entry method used for testing purposes.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BigDecimal returnValue;
+		BigDecimal returnValue = null;
 
 		CWITest cwiTest = new CWITest();
 		try {
-			returnValue = cwiTest.currencyQuotation("UDS", "BRL", 12, "18/09/2017");
+			returnValue = cwiTest.currencyQuotation("USD", "EUR", 100.00, "20/11/2014");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		System.out.println(returnValue);
 	}
 
 	/**
@@ -115,15 +133,38 @@ public class CWITest {
 
 		File csvFile = retrieveQuotationCsvFile(quotationDate);
 
-		return null;
+		Map<String, CurrencyExchangingInformation> currencyExchangingInformationMap;
+		currencyExchangingInformationMap = createExchangingInformationMap(csvFile);
+
+		csvFile.delete();
+
+		CurrencyExchangingInformation currencyExchangingInformationFrom = currencyExchangingInformationMap.get(from);
+		if (null == currencyExchangingInformationFrom) {
+			throw new RuntimeException("Could not find exchanging rates for \"" + from + "\" currency.");
+		}
+
+		CurrencyExchangingInformation currencyExchangingInformationTo = currencyExchangingInformationMap.get(to);
+		if (null == currencyExchangingInformationTo) {
+			throw new RuntimeException("Could not find exchanging rates for \"" + to + "\" currency.");
+		}
+
+		double exchangeRate = (currencyExchangingInformationFrom.getBuyingRate()
+				/ currencyExchangingInformationTo.getBuyingRate());
+		double convertedValue = Math.round((value.doubleValue() * exchangeRate) * 100.0) / 100.0;
+		BigDecimal convertedValueBigDecimal = new BigDecimal(Double.toString(convertedValue));
+
+		return convertedValueBigDecimal;
 	}
 
 	/**
-	 * TODO: Document.
+	 * Retrieves the Real quotation CSV file of a specific date from brazilian
+	 * central bank.
 	 * 
 	 * @param quotationDate
-	 * @return
+	 *            Date from which the quotation CSV file should be retrieved.
+	 * @return The specified date's quotation CSV file.
 	 * @throws IOException
+	 *             When the quotation CSV file could not be retrieved.
 	 */
 	private final File retrieveQuotationCsvFile(Date quotationDate) throws IOException {
 
@@ -200,5 +241,80 @@ public class CWITest {
 		}
 
 		return csvFile;
+	}
+
+	/**
+	 * creates a {@link HashMap} with the currencies' exchanging information based
+	 * on the values read from a CSV file.
+	 * 
+	 * @param csvFile
+	 *            The CSV file from which the currencies' exchanging information
+	 *            will be retrieved.
+	 * 
+	 * @return A {@link HashMap} with the currencies' exchanging information
+	 */
+	private final Map<String, CurrencyExchangingInformation> createExchangingInformationMap(File csvFile)
+			throws IOException {
+		Map<String, CurrencyExchangingInformation> currencyExchangingInformationMap = new HashMap<>();
+
+		/* Opens the CSV file scanner. */
+		Scanner csvFileScanner;
+		try {
+			csvFileScanner = new Scanner(csvFile);
+		} catch (FileNotFoundException fileNotFoundException) {
+			throw new IOException("Could not find quotation CSV file \"" + csvFile + "\".", fileNotFoundException);
+		}
+
+		String lineBuffer;
+		String[] values;
+		Date exchangingDate;
+		String currencyCode;
+		String type;
+		String currencyAbbreviation = null;
+		Double buyingRate;
+		Double sellingRate;
+		Double buyingPpp;
+		Double sellingPpp;
+		CurrencyExchangingInformation currencyExchangingInformation;
+
+		/*
+		 * Since we're reading values acquired from a brazilian bank, the number parser
+		 * must read then on the brazilian format (i. e. the decimal separator character
+		 * is comma and the grouping separator character is dot).
+		 */
+		DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+		decimalFormatSymbols.setDecimalSeparator(',');
+		decimalFormatSymbols.setGroupingSeparator('.');
+
+		DecimalFormat decimalFormat = new DecimalFormat();
+		decimalFormat.setDecimalFormatSymbols(decimalFormatSymbols);
+
+		try {
+			while (csvFileScanner.hasNextLine()) {
+				lineBuffer = csvFileScanner.nextLine();
+				values = lineBuffer.split(CSV_SEPARATION_CHARACTER);
+				currencyAbbreviation = values[3];
+				currencyCode = values[1];
+				type = values[2];
+				buyingRate = decimalFormat.parse(values[4]).doubleValue();
+				sellingRate = decimalFormat.parse(values[5]).doubleValue();
+				buyingPpp = decimalFormat.parse(values[6]).doubleValue();
+				sellingPpp = decimalFormat.parse(values[7]).doubleValue();
+				exchangingDate = new SimpleDateFormat("dd/MM/yyyy").parse(values[0]);
+
+				currencyExchangingInformation = new CurrencyExchangingInformation(exchangingDate, currencyCode, type,
+						currencyAbbreviation, buyingRate, sellingRate, buyingPpp, sellingPpp);
+
+				currencyExchangingInformationMap.put(currencyExchangingInformation.getCurrencyAbbreviation(),
+						currencyExchangingInformation);
+			}
+		} catch (ParseException parseException) {
+			throw new IOException("Could not parse an information for currency \"" + currencyAbbreviation
+					+ "\" from CSV file \"" + csvFile.getAbsolutePath() + "\".", parseException);
+		} finally {
+			csvFileScanner.close();
+		}
+
+		return currencyExchangingInformationMap;
 	}
 }
---
+++ cwiTest/src/org/marceloleite/cwitest/CurrencyExchangingInformation.java
@@ -0,0 +1,148 @@
+package org.marceloleite.cwitest;
+
+import java.util.Date;
+
+/**
+ * Exchanging rates information from brazilian Real to another currency on a
+ * given date.
+ * 
+ * @author Marcelo Leite
+ *
+ */
+public class CurrencyExchangingInformation {
+
+	/**
+	 * The exchanging date.
+	 */
+	private Date exchangingDate;
+
+	/**
+	 * The currency identification code.
+	 */
+	private String currencyCode;
+
+	/**
+	 * The currency type.
+	 */
+	private String type;
+
+	/**
+	 * The currency abbreviation.
+	 */
+	private String currencyAbbreviation;
+
+	/**
+	 * The currency's buying rate.
+	 */
+	private double buyingRate;
+
+	/**
+	 * The currency's selling rate.
+	 */
+	private double sellingRate;
+
+	/**
+	 * The currency's buying purchasing power parity
+	 */
+	private double buyingPpp;
+
+	/**
+	 * The currency's selling purchasing power parity
+	 */
+	private double sellingPpp;
+
+	public CurrencyExchangingInformation(Date exchangingDate, String currencyCode, String type,
+			String currencyAbbreviation, double buyingRate, double sellingRate, double buyingPpp, double sellingPpp) {
+		this.exchangingDate = exchangingDate;
+		this.currencyCode = currencyCode;
+		this.type = type;
+		this.currencyAbbreviation = currencyAbbreviation;
+		this.buyingRate = buyingRate;
+		this.sellingRate = sellingRate;
+		this.buyingPpp = buyingPpp;
+		this.sellingPpp = sellingPpp;
+	}
+
+	/**
+	 * Returns the exchanging date.
+	 * 
+	 * @return The exchanging date.
+	 */
+	public final Date getExchangingDate() {
+		return exchangingDate;
+	}
+
+	/**
+	 * Returns the currency identification code.
+	 * 
+	 * @return The currency identification code.
+	 */
+	public final String getCurrencyCode() {
+		return currencyCode;
+	}
+
+	/**
+	 * Returns the currency type.
+	 * 
+	 * @return The currency type.
+	 */
+	public final String getType() {
+		return type;
+	}
+
+	/**
+	 * Returns the currency abbreviation.
+	 * 
+	 * @return The currency abbreviation.
+	 */
+	public final String getCurrencyAbbreviation() {
+		return currencyAbbreviation;
+	}
+
+	/**
+	 * Returns the currency buying rate.
+	 * 
+	 * @return The currency buying rate.
+	 */
+	public final double getBuyingRate() {
+		return buyingRate;
+	}
+
+	/**
+	 * Returns the currency selling rate.
+	 * 
+	 * @return The currency selling rate.
+	 */
+	public final double getSellingRate() {
+		return sellingRate;
+	}
+
+	/**
+	 * Returns the currency's buying purchasing power parity
+	 * 
+	 * @return The currency's buying purchasing power parity
+	 */
+	public final double getBuyingPpp() {
+		return buyingPpp;
+	}
+
+	/**
+	 * Returns the currency's selling purchasing power parity
+	 * 
+	 * @return The currency's selling purchasing power parity
+	 */
+	public final double getSellingPpp() {
+		return sellingPpp;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append(currencyAbbreviation + " - ");
+		stringBuffer.append("Type: " + type + ", ");
+		stringBuffer.append("buying rate: " + buyingRate + ", ");
+		stringBuffer.append("buying ppp: " + buyingPpp + ".");
+		return stringBuffer.toString();
+	}
+
+}
