b4d6f8623cd89bcf2cffe83d55fe3f5503894d34
MarceloLeite2604 <marceloleite2604@gmail.com>
2017-09-22 15:39:24 -0300

    Creating initial test cases.

---
+++ cwiTest/src/test/java/org/marceloleite/cwiTest/CWITestTest.java
@@ -0,0 +1,63 @@
+package org.marceloleite.cwiTest;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.InvalidParameterException;
+
+import org.junit.Test;
+import org.marceloleite.cwitest.CWITest;
+
+public class CWITestTest {
+	
+	@Test(expected = InvalidParameterException.class)
+	public void testCurrencyQuotationEmptyFromInvalidParameter() {
+		try {
+			new CWITest().currencyQuotation("", "EUR", 100.00, "20/09/2017");
+		} catch (IOException ioException) {
+			fail("IOException thrown: " + ioException.getMessage());
+		}
+		fail("An InvalidParameterException must be thrown.");
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void testCurrencyQuotationNullFromInvalidParameter() {
+		try {
+			new CWITest().currencyQuotation(null, "EUR", 100.00, "20/09/2017");
+		} catch (IOException ioException) {
+			fail("IOException thrown: " + ioException.getMessage());
+		}
+		fail("An InvalidParameterException must be thrown.");
+	}
+	
+	@Test(expected = InvalidParameterException.class)
+	public void testCurrencyQuotationEmptyToInvalidParameter() {
+		try {
+			new CWITest().currencyQuotation("USD", "", 100.00, "20/09/2017");
+		} catch (IOException ioException) {
+			fail("IOException thrown: " + ioException.getMessage());
+		}
+		fail("An InvalidParameterException must be thrown.");
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void testCurrencyQuotationNullToInvalidParameter() {
+		try {
+			new CWITest().currencyQuotation("USD", null, 100.00, "20/09/2017");
+		} catch (IOException ioException) {
+			fail("IOException thrown: " + ioException.getMessage());
+		}
+		fail("An InvalidParameterException must be thrown.");
+	}
+	
+	@Test(expected = InvalidParameterException.class)
+	public void testCurrencyQuotationValueSmallerThanZeroInvalidParameter() {
+		try {
+			new CWITest().currencyQuotation("USD", "EUR", -2, "20/09/2017");
+		} catch (IOException ioException) {
+			fail("IOException thrown: " + ioException.getMessage());
+		}
+		fail("An InvalidParameterException must be thrown.");
+	}
+
+}
